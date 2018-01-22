1e24959e38f80034a0929e113e628ad24e8d6201
Piotr Płaczek <piotr.placzek@zeg-energetyka.pl>
2018-01-22 14:25:06 +0100

    Set up default user name and password.

--- Engine/Models/SignUserModel.php
+++ Engine/Models/SignUserModel.php
@@ -1,7 +1,18 @@
 <?php
 class SignUserModel{
+    private const $__defaultUser = "root";
+    private const $__defaultPass = "root";
+
     function _construct(){
 
+    }
+
+    public function user(){
+        return $__defaultUser;
+    }
+
+    public function password(){
+        return $__defaultPass;
     }
 }
 ?>
