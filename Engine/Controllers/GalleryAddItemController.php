db5eed5412e4f2dd917eb76f5287237dd7559292
Piotr Płaczek <piotr.placzek@zeg-energetyka.pl>
2018-01-22 15:14:50 +0100

    The definition of the GalleryAddItemController has started.

--- Engine/Controllers/GalleryAddItemController.php
+++ Engine/Controllers/GalleryAddItemController.php
@@ -1,7 +1,29 @@
 <?php
+include "SignInController.php"
+include "GalleryController.php"
+include "./Models/GalleryItemModel.php"
+include "./Views/GalleryAddItemView.php"
+
+
 class GalleryAddItemController{
     function _construct(){
+        session_start();
+        $signInController = new SignInController();
+        if(!$signInController->isUserSignedIn()){
+            echo '<div class="acces-denied>Brak uprawnień do oglądania tej strony</div>';
+        }
+        else if(isset($_POST["additem"])){
+            //upload file
+            //create gallery item
+            //add item to gallery
+            //update database
+        }
+        else{
+            $view = new GalleryAddItemView();
+        }
+    }
 
+    private function parseNewGalleryItem(){
     }
 }
 ?>
