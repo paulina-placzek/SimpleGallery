aceaa5e8866b921624eeff25390c7ba68dc22af2
Piotr Płaczek <piotr.placzek@zeg-energetyka.pl>
2018-01-22 14:57:56 +0100

    Defined SignInController.

--- Engine/Controllers/SignInController.php
+++ Engine/Controllers/SignInController.php
@@ -1,7 +1,38 @@
 <?php
+include "./Views/SignInView.php"
 class SignInController{
     function _construct(){
+    }
 
+    public function initialize($signInPageUrl){
+        session_start();
+        if(isUserSignedIn()){
+            return true;
+        }
+        else{
+            if(isset($_POST["signin"])){
+                if(signIn()){
+                    return true;
+                }
+            }
+            $view = new SignInView($signInPageUrl);
+            return false;
+        }
+    }
+
+    public function isUserSignedIn(){
+        return isset($_SESSION["uisi"]);
+    }
+
+    private function signIn(){
+        if(isset($_POST["user"]) && isset($_POST["pass"])){
+            $default_user = new SignUserModel();
+            if($_POST["user"] == $default_user->user() && $_POST["pass"] == $default_user->password()){
+                $_SESSION["uisi"] = 1;
+                return true;
+            }
+        }
+        return false;
     }
 }
 ?>
